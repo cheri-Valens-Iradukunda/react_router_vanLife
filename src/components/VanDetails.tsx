fc882a28f2687175ca411614d57f4f56fb86bbda
valens <iradukundacherivalens88@gmail.com>
2026-03-03 20:41:03 +0200

    page for showing single clicked van

--- src/components/VanDetails.tsx
+++ src/components/VanDetails.tsx
@@ -1,23 +1,31 @@
 import { useEffect, useState, type JSX } from "react";
+import { Link, useParams } from "react-router-dom";
+
 
 export const VanDetails = (): JSX.Element => {
-
-    const [singleData,setSingleData] = useState()
-
-    const fetchData = async() => {
-        const id = 1
-
-        await fetch(`/api/fetch/${id}`).then(res=>res.json()).then(res=>setSingleData(res))
-        
-
+    const [singleVan,setSingleVan] = useState({
+        description:"",
+        id:"",
+        imageUrl:"",
+        name:"",
+        price:0,
+        type:""
+    })
+    const params = useParams()
+    const fetchData = ():void =>{
+        fetch("/api/vans/"+params['id']).then(res=>res.json()).then(res=>setSingleVan(res['vans']))
     }
 
-    useEffect(()=>{
-        fetchData()
-    },[])
-    console.log(singleData)
+    useEffect(()=>fetchData(),[])
 
-    return <div>
-        <h2>this is van Details</h2>
+    return <div className="p-10 space-y-5">
+        <Link to="/vans" className="underline">back to all vans</Link>
+        <img src={singleVan['imageUrl']} alt=" van image" />
+        <p className="bg-[#FF8C38] px-3 py-1 w-fit text-white rounded">{singleVan["type"]}</p>
+        <h3 className="font-bold text-3xl">{singleVan["name"]}</h3>
+        <p className="text-xl"><span className="font-bold">${singleVan["price"]}</span> /day</p>
+        <p>{singleVan["description"]}</p>
+        <button className="bg-[#FF8C38] w-full text-white py-3 rounded">Rent this van</button>
+
     </div>
 }
