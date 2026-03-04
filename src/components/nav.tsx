d6126b366746e16e4ea292fd75e04e3f6b9d67c7
valens <iradukundacherivalens88@gmail.com>
2026-03-04 20:49:37 +0200

    adding other links for new pages

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,13 +1,14 @@
 import type React from "react"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 export const Nav = ():React.ReactElement => {
     return (
         <nav className="flex justify-between p-10 items-center">
             <h2 className="uppercase text-2xl font-bold">#vanlife</h2>
-            <ul className="flex gap-5 font-semibold text-gray-600">
-                <Link to="/about">About</Link>
-                <Link to="/vans">Vans</Link>
+            <ul className="flex gap-5 font-semibold text-gray-600 *:hover:underline *:hover:text-gray-300">
+                <NavLink className={({isActive}) => isActive? "text-black underline": ""} to="about">About</NavLink>
+                <NavLink className={({isActive}) => isActive? "text-black underline": ""} to="vans">Vans</NavLink>
+                <NavLink className={({isActive}) => isActive? "text-black underline": ""} to="host">Host</NavLink>
             </ul>
         </nav>
     )
