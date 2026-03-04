48266a0e7f3307aa99e0f9a4c6b8b4728e9b5fa0
valens <iradukundacherivalens88@gmail.com>
2026-03-04 20:49:04 +0200

    adding other routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import { Footer } from './components/Footer'
-// import { HomeBody } from './components/HomeBody'
-import { Nav } from './components/nav'
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AboutPage } from './components/Page2Body'
 import { HomeBody } from './components/HomeBody'
 import { createContext, useEffect, useState } from 'react'
 import { Vans } from './components/Vans'
 import { VanDetails } from './components/VanDetails'
+import { Dashboard } from './components/hosts/Dashboard'
+import { Income } from './components/hosts/Income'
+import { Reviews } from './components/hosts/Review'
+import { HostNav } from './components/hosts/HostNav'
+import { Layout } from './components/Layout/Layout'
+import { HostedVans } from './components/hosts/ListedVans'
+import { HostedVanDetails } from './components/hosts/HostVanDetails'
+// import { Description } from './components/hosts/Descri'
 
-// type vansValue = {
-//   description:string
-//   id:string
-//   imageUrl:string
-//   name:string
-//   price:number
-//   type:string
-// }
+export type vansValue = {
+  description:string
+  id:string
+  imageUrl:string
+  name:string
+  price:number
+  type:string
+  hostId: string
+}
 
 export const Context = createContext([]);
 
@@ -36,14 +43,33 @@ function App() {
     <div className='md:w-[50vw] max-sm:w-screen sm:w-[80vw] mx-auto bg-white'>
       <Context.Provider value={vans}>
       <BrowserRouter>
-        <Nav />
           <Routes>
-              <Route path='/' element={<HomeBody />} />
-              <Route path='/vans' element={<Vans />} />
-            <Route path='/about' element={<AboutPage />} />
-            <Route path='/vans/:id' element={<VanDetails />} />
+            <Route path='/' element={<Layout />}>
+
+              <Route index element={<HomeBody />} />
+              <Route path='about' element={<AboutPage />} />
+
+              <Route path='vans'>
+                <Route index element={<Vans />} />
+                <Route path=':id' element={<VanDetails />} />
+              </Route>
+              
+              <Route path="host" element={<HostNav />} >
+                <Route index element={<Dashboard />} />
+                <Route path="income" element={<Income />} />
+                <Route path="review" element={<Reviews />} />
+                <Route path="vans" element={<HostedVans />} />
+
+                <Route path="vans/:id" element={<HostedVanDetails />} >
+                  <Route index/>
+                  <Route path='images' element={<h2>images goes here</h2>} />
+                  <Route path='price' element={<h2>pricing goes here</h2>} />
+                </Route>
+              
+              </Route>
+            </Route>
+
           </Routes>
-        <Footer />
       </BrowserRouter>
       </Context.Provider>
     </div>
