863dddfc8920220d7c85f806228915aa57876882
valens <iradukundacherivalens88@gmail.com>
2026-03-05 23:29:40 +0200

    finalizing the second scrimba progress

--- src/components/hosts/VanDescription.tsx
+++
@@ -1,9 +0,0 @@
-// import { useOutletContext } from "react-router-dom"
-
-export const VanDescription = () => {
-
-    // const {singleVan} = useOutletContext()
-
-    return <div>this is description page</div>
-
-}
