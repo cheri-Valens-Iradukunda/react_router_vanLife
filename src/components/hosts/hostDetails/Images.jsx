7d0c0df1064fe9b0df6955a0e74ac02e8e175bd5
valens <iradukundacherivalens88@gmail.com>
2026-03-05 23:33:34 +0200

    playing around to see progress

--- src/components/hosts/hostDetails/Images.jsx
+++ src/components/hosts/hostDetails/Images.jsx
@@ -2,6 +2,7 @@ import { useOutletContext } from "react-router-dom"
 
 export const Images = () => {
     const {singleVan} = useOutletContext()
+    console.log(singleVan)
     return <div>
         <img src={singleVan.imageUrl} className="w-1/2" />
     </div>
