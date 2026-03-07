145e96bc3fa5e7809b6d5a7cf6787c60c4e40530
valens <iradukundacherivalens88@gmail.com>
2026-03-07 17:15:30 +0200

    removing unnecessary lines

--- src/components/hosts/hostDetails/Prices.jsx
+++ src/components/hosts/hostDetails/Prices.jsx
@@ -3,7 +3,6 @@ import { useOutletContext } from "react-router-dom"
 export const Prices = () => {
 
     const {singleVan} = useOutletContext()
-    // console.log(s)
     return <div>
          <h3 className="font-bold">${singleVan.price}<span>/day</span></h3>
     </div>
