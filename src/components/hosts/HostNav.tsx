e355b4090a2261dbe58f7365c4ad8c4b2910d91f
valens <iradukundacherivalens88@gmail.com>
2026-03-13 17:12:34 +0200

    changing navs components

---
+++ src/components/hosts/HostNav.tsx
@@ -0,0 +1,40 @@
+import {  NavLink, Outlet } from "react-router-dom";
+
+export const HostNav = () => {
+
+    const destinations:string[] = [".","income","vans","review"]
+    const texts:string[] = ["Dashboard","Income","vans","Review"]
+    
+    return <div>
+        <nav className="flex gap-3 *:hover:underline *:hover:text-gray-300 text-gray-700">
+            {
+                texts.map((elem,index)=>{
+                    return NavLinks(elem,destinations[index])
+                })
+            }
+        </nav>
+        <Outlet />
+    </div>
+}
+
+export const HostVanDetailsNav = () => {
+    let texts = ["Details","pricing","Photos"]
+    let destinations = [".", "price","images"]
+    return <div>
+        <nav className="flex gap-3 *:hover:underline *:hover:text-gray-300 text-gray-700">
+
+            {
+                texts.map((elem,index) => {
+                    return NavLinks(elem,destinations[index])
+                })
+            }
+
+        </nav>
+    </div>
+}
+
+function NavLinks(texts:string,destinations:string){
+
+    return <NavLink key={texts} className={({isActive}) => isActive ? "text-black underline":""} end to={destinations}>{texts}</NavLink>
+    
+}
