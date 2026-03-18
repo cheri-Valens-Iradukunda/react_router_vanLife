d710ac8b5602dab8845ca5eee81b2ac826e812aa
valens <iradukundacherivalens88@gmail.com>
2026-03-18 15:58:06 +0200

    addding more functions to fetch data

--- src/components/Api.ts
+++ src/components/Api.ts
@@ -13,3 +13,52 @@ export const GetVans = async (): Promise<vansValue[]> => {
     const data = await res.json()
     return data.vans
 }
+
+
+export async function getVans(id:string|number) {
+    const url = `/api/vans/${id}`
+    const res = await fetch(url)
+    if (!res.ok) {
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json()
+    return data.vans
+}
+
+export async function getHostVans(){
+    const url = "/api/host/vans"
+
+    const res = await fetch(url)
+
+    if(!res.ok){
+        throw {
+            message:"Failed to fetch vans",
+            status: res.status,
+            statusText: res.statusText
+        }
+    }
+    const data = await res.json()
+    return data.vans
+
+}
+
+export async function loginUser(creds:any) {
+    const res = await fetch("/api/login",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
+    console.log(data)
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+
+    return data
+}
